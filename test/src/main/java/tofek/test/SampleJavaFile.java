1b2ba15f3b7c6219d99ac7fa3f092643779f0148
tkhan1987 <112979035+tkhan1987@users.noreply.github.com>
2022-09-08 14:34:03 -0500

    Git Jenkins Integration

--- test/src/main/java/tofek/test/SampleJavaFile.java
+++ test/src/main/java/tofek/test/SampleJavaFile.java
@@ -3,7 +3,7 @@ package tofek.test;
 public class SampleJavaFile {
 	
 	public static void main(String[] args) {
-		System.out.println("This is Tofek KHan");
+		System.out.println("This is Tofek KHan after build code with Jenkins");
 	}
 
 }
