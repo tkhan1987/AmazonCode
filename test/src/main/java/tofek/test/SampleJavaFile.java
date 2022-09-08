3e9d19ad825c8ca491b5917894ae52b7052aae0b
tkhan1987 <112979035+tkhan1987@users.noreply.github.com>
2022-09-08 16:04:06 -0500

    Update

--- test/src/main/java/tofek/test/SampleJavaFile.java
+++ test/src/main/java/tofek/test/SampleJavaFile.java
@@ -3,7 +3,7 @@ package tofek.test;
 public class SampleJavaFile {
 	
 	public static void main(String[] args) {
-		System.out.println("This is Tofek KHan after build code with Jenkins");
+		System.out.println("This is Tofek KHan after build code with Jenkins Final Version");
 	}
 
 }
